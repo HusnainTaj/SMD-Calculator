f13539dfa64cd14b7eb55f8aa4377dc7157184b4
Husnain Taj <husnain.taj@gmail.com>
2025-02-18 23:59:00 +0500

    Implemented Rest of the calculator buttons

--- app/src/main/java/com/taj/smd/a1/calculator/MainActivity.java
+++ app/src/main/java/com/taj/smd/a1/calculator/MainActivity.java
@@ -2,7 +2,6 @@ package com.taj.smd.a1.calculator;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,25 +10,38 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
+
+
 @SuppressLint("SetTextI18n")
 public class MainActivity extends AppCompatActivity
 {
+    DecimalFormat df = new DecimalFormat("0.##########");
+
     TextView tvDisplay, tvResult;
 
     // State
-    int PreviousNumber = 0; // previous number
-    int number = 0; // current number
-    void setNumber(int num)
+    double PreviousNumber = 0; // previous number
+    double number = 0; // current number
+    void setNumber(double num)
     {
-        number = number * 10 + num;
+        number = num;
+        numberStr = df.format(number);
+        tvResult.setText(numberStr);
+    }
+    String numberStr = ""; // current number as string
+    void appendNum(int num)
+    {
+        numberStr += num;
+        setNumber(Double.parseDouble(numberStr));
 
         if (PreviousNumber != 0)
-            tvDisplay.setText(PreviousNumber + " " + operator);
-        tvResult.setText(String.valueOf(number));
+            tvDisplay.setText(df.format(PreviousNumber) + " " + operator);
     }
     String operator = ""; // current operator
     void setOperator(String op)
     {
+//         If there is a previous number and current number, calculate the result first
         if(PreviousNumber != 0 && number != 0)
             handleEqualClick();
 
@@ -37,14 +49,13 @@ public class MainActivity extends AppCompatActivity
         if (PreviousNumber == 0)
             PreviousNumber = number;
 
-        number = 0;
-        tvDisplay.setText(PreviousNumber + " " + operator);
-        tvResult.setText("0");
+        setNumber(0);
+        tvDisplay.setText(df.format(PreviousNumber )+ " " + operator);
     }
 
     void handleEqualClick()
     {
-        int result = 0;
+        double result = 0;
         switch (operator)
         {
             case "+":
@@ -60,9 +71,8 @@ public class MainActivity extends AppCompatActivity
                 result = PreviousNumber / number;
                 break;
         }
-        tvDisplay.setText(PreviousNumber + " " + operator + " " + number + " =");
-        tvResult.setText(String.valueOf(result));
-        number = result;
+        tvDisplay.setText(df.format(PreviousNumber) + " " + operator + " " + df.format(number) + " =");
+        setNumber(result);
         PreviousNumber = 0;
         operator = "";
     }
@@ -84,16 +94,16 @@ public class MainActivity extends AppCompatActivity
         tvResult = findViewById(R.id.tvResult);
 
         // Set onClickListener for number buttons
-        findViewById(R.id.num0).setOnClickListener(v -> setNumber(0));
-        findViewById(R.id.num1).setOnClickListener(v -> setNumber(1));
-        findViewById(R.id.num2).setOnClickListener(v -> setNumber(2));
-        findViewById(R.id.num3).setOnClickListener(v -> setNumber(3));
-        findViewById(R.id.num4).setOnClickListener(v -> setNumber(4));
-        findViewById(R.id.num5).setOnClickListener(v -> setNumber(5));
-        findViewById(R.id.num6).setOnClickListener(v -> setNumber(6));
-        findViewById(R.id.num7).setOnClickListener(v -> setNumber(7));
-        findViewById(R.id.num8).setOnClickListener(v -> setNumber(8));
-        findViewById(R.id.num9).setOnClickListener(v -> setNumber(9));
+        findViewById(R.id.num0).setOnClickListener(v -> appendNum(0));
+        findViewById(R.id.num1).setOnClickListener(v -> appendNum(1));
+        findViewById(R.id.num2).setOnClickListener(v -> appendNum(2));
+        findViewById(R.id.num3).setOnClickListener(v -> appendNum(3));
+        findViewById(R.id.num4).setOnClickListener(v -> appendNum(4));
+        findViewById(R.id.num5).setOnClickListener(v -> appendNum(5));
+        findViewById(R.id.num6).setOnClickListener(v -> appendNum(6));
+        findViewById(R.id.num7).setOnClickListener(v -> appendNum(7));
+        findViewById(R.id.num8).setOnClickListener(v -> appendNum(8));
+        findViewById(R.id.num9).setOnClickListener(v -> appendNum(9));
 
         // Set onClickListener for operator buttons
         findViewById(R.id.plus).setOnClickListener(v -> setOperator("+"));
@@ -106,50 +116,56 @@ public class MainActivity extends AppCompatActivity
             if (number != 0)
             {
                 number *= number;
-                tvResult.setText(String.valueOf(number));
+                tvResult.setText(df.format(number));
             }
         });
 
         findViewById(R.id.sqrt).setOnClickListener(v -> {
             if (number != 0)
             {
-                number = (int) Math.sqrt(number);
-                tvResult.setText(String.valueOf(number));
+                setNumber(Math.sqrt(number));
             }
         });
 
         findViewById(R.id.percent).setOnClickListener(v -> {
             if (number != 0)
             {
-                number = PreviousNumber * number / 100;
-                tvResult.setText(String.valueOf(number));
+                setNumber(PreviousNumber * number / 100);
             }
         });
 
         findViewById(R.id.negate).setOnClickListener(v -> {
             if (number != 0)
             {
-                number = -number;
-                tvResult.setText(String.valueOf(number));
+                setNumber(-number);
             }
         });
 
-        // TODO: Implement Floating Point
         findViewById(R.id.inverse).setOnClickListener(v -> {
             if (number != 0)
             {
-                number = 1 / number;
-                tvResult.setText(String.valueOf(number));
+                setNumber(1 / number);
+            }
+        });
+
+        findViewById(R.id.dot).setOnClickListener(v -> {
+            if (!numberStr.contains("."))
+            {
+                numberStr += ".";
+                tvResult.setText(numberStr);
             }
         });
 
         // Set onClickListener for clear button
         findViewById(R.id.clear).setOnClickListener(v -> {
             tvDisplay.setText("");
-            number = 0;
+            setNumber(0);
             operator = "";
             PreviousNumber = 0;
-            tvResult.setText("");
+        });
+
+        findViewById(R.id.clearEntry).setOnClickListener(v -> {
+            setNumber(0);
         });
 
         // Set onClickListener for backspace button
@@ -158,8 +174,8 @@ public class MainActivity extends AppCompatActivity
             if (number != 0)
             {
                 number /= 10;
-                tvDisplay.setText(String.valueOf(number));
-                tvResult.setText(String.valueOf(number));
+                tvDisplay.setText(df.format(number));
+                tvResult.setText(df.format(number));
             }
         });
 
