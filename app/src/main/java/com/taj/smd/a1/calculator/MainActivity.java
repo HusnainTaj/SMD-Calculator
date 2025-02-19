a071e56ca3c204e9b1e9c42b2159e0f3a1394e59
Husnain Taj <husnain.taj@gmail.com>
2025-02-19 21:43:32 +0500

    Responsiveness and final

--- app/src/main/java/com/taj/smd/a1/calculator/MainActivity.java
+++ app/src/main/java/com/taj/smd/a1/calculator/MainActivity.java
@@ -50,11 +50,19 @@ public class MainActivity extends AppCompatActivity
             PreviousNumber = number;
 
         setNumber(0);
+        tvResult.setText("");
         tvDisplay.setText(df.format(PreviousNumber )+ " " + operator);
     }
 
     void handleEqualClick()
     {
+        if (operator.isEmpty())
+            return;
+
+        if(PreviousNumber == 0 || number == 0)
+            return;
+
+
         double result = 0;
         switch (operator)
         {
@@ -81,7 +89,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) ->
         {
@@ -93,26 +101,28 @@ public class MainActivity extends AppCompatActivity
         tvDisplay = findViewById(R.id.tvDisplay);
         tvResult = findViewById(R.id.tvResult);
 
+
         // Set onClickListener for number buttons
-        findViewById(R.id.num0).setOnClickListener(v -> appendNum(0));
-        findViewById(R.id.num1).setOnClickListener(v -> appendNum(1));
-        findViewById(R.id.num2).setOnClickListener(v -> appendNum(2));
-        findViewById(R.id.num3).setOnClickListener(v -> appendNum(3));
-        findViewById(R.id.num4).setOnClickListener(v -> appendNum(4));
-        findViewById(R.id.num5).setOnClickListener(v -> appendNum(5));
-        findViewById(R.id.num6).setOnClickListener(v -> appendNum(6));
-        findViewById(R.id.num7).setOnClickListener(v -> appendNum(7));
-        findViewById(R.id.num8).setOnClickListener(v -> appendNum(8));
-        findViewById(R.id.num9).setOnClickListener(v -> appendNum(9));
+        findViewById(R.id.num0).setOnClickListener(v -> {v.performHapticFeedback(1);appendNum(0);});
+        findViewById(R.id.num1).setOnClickListener(v -> {v.performHapticFeedback(1);appendNum(1);});
+        findViewById(R.id.num2).setOnClickListener(v -> {v.performHapticFeedback(1);appendNum(2);});
+        findViewById(R.id.num3).setOnClickListener(v -> {v.performHapticFeedback(1);appendNum(3);});
+        findViewById(R.id.num4).setOnClickListener(v -> {v.performHapticFeedback(1);appendNum(4);});
+        findViewById(R.id.num5).setOnClickListener(v -> {v.performHapticFeedback(1);appendNum(5);});
+        findViewById(R.id.num6).setOnClickListener(v -> {v.performHapticFeedback(1);appendNum(6);});
+        findViewById(R.id.num7).setOnClickListener(v -> {v.performHapticFeedback(1);appendNum(7);});
+        findViewById(R.id.num8).setOnClickListener(v -> {v.performHapticFeedback(1);appendNum(8);});
+        findViewById(R.id.num9).setOnClickListener(v -> {v.performHapticFeedback(1);appendNum(9);});
 
         // Set onClickListener for operator buttons
-        findViewById(R.id.plus).setOnClickListener(v -> setOperator("+"));
-        findViewById(R.id.minus).setOnClickListener(v -> setOperator("-"));
-        findViewById(R.id.multiply).setOnClickListener(v -> setOperator("*"));
-        findViewById(R.id.divide).setOnClickListener(v -> setOperator("/"));
+        findViewById(R.id.plus).setOnClickListener(v -> {v.performHapticFeedback(1);setOperator("+");});
+        findViewById(R.id.minus).setOnClickListener(v -> {v.performHapticFeedback(1);setOperator("-");});
+        findViewById(R.id.multiply).setOnClickListener(v -> {v.performHapticFeedback(1);setOperator("*");});
+        findViewById(R.id.divide).setOnClickListener(v -> {v.performHapticFeedback(1);setOperator("/");});
 
         // Set onClickListener for Advanced buttons
         findViewById(R.id.square).setOnClickListener(v -> {
+            v.performHapticFeedback(1);
             if (number != 0)
             {
                 number *= number;
@@ -121,6 +131,7 @@ public class MainActivity extends AppCompatActivity
         });
 
         findViewById(R.id.sqrt).setOnClickListener(v -> {
+            v.performHapticFeedback(1);
             if (number != 0)
             {
                 setNumber(Math.sqrt(number));
@@ -128,6 +139,7 @@ public class MainActivity extends AppCompatActivity
         });
 
         findViewById(R.id.percent).setOnClickListener(v -> {
+            v.performHapticFeedback(1);
             if (number != 0)
             {
                 setNumber(PreviousNumber * number / 100);
@@ -135,6 +147,7 @@ public class MainActivity extends AppCompatActivity
         });
 
         findViewById(R.id.negate).setOnClickListener(v -> {
+            v.performHapticFeedback(1);
             if (number != 0)
             {
                 setNumber(-number);
@@ -142,6 +155,7 @@ public class MainActivity extends AppCompatActivity
         });
 
         findViewById(R.id.inverse).setOnClickListener(v -> {
+            v.performHapticFeedback(1);
             if (number != 0)
             {
                 setNumber(1 / number);
@@ -149,6 +163,7 @@ public class MainActivity extends AppCompatActivity
         });
 
         findViewById(R.id.dot).setOnClickListener(v -> {
+            v.performHapticFeedback(1);
             if (!numberStr.contains("."))
             {
                 numberStr += ".";
@@ -158,6 +173,7 @@ public class MainActivity extends AppCompatActivity
 
         // Set onClickListener for clear button
         findViewById(R.id.clear).setOnClickListener(v -> {
+            v.performHapticFeedback(1);
             tvDisplay.setText("");
             setNumber(0);
             operator = "";
@@ -165,22 +181,26 @@ public class MainActivity extends AppCompatActivity
         });
 
         findViewById(R.id.clearEntry).setOnClickListener(v -> {
+            v.performHapticFeedback(1);
             setNumber(0);
         });
 
         // Set onClickListener for backspace button
         findViewById(R.id.backspace).setOnClickListener(v ->
         {
-            if (number != 0)
+            v.performHapticFeedback(1);
+            if (numberStr.length() > 0)
             {
-                number /= 10;
-                tvDisplay.setText(df.format(number));
-                tvResult.setText(df.format(number));
+                numberStr = numberStr.substring(0, numberStr.length() - 1);
+                if (numberStr.length() > 0)
+                    setNumber(Double.parseDouble(numberStr));
+                else
+                    setNumber(0);
             }
         });
 
         // Set onClickListener for equal button
-        findViewById(R.id.equal).setOnClickListener(v -> handleEqualClick());
+        findViewById(R.id.equal).setOnClickListener(v -> {v.performHapticFeedback(1);handleEqualClick();});
 
     }
 }
